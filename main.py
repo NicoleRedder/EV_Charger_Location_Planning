3493000fd9401a8371bb35912bbb30e759e3d1e2
NicoleRedder <139513971+NicoleRedder@users.noreply.github.com>
2024-12-07 15:54:01 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,261 @@
+
+# -*- coding: utf-8 -*-
+
+#included for convenience, as this was previously used
+import random 
+#floor function
+import math
+#modeling
+from gurobipy import GRB,Model,quicksum
+#not needed but included for convenience
+import gurobipy
+#data reading & writing
+import pandas as pd
+import geopandas as gpd
+#previously used, included for convenience
+from shapely.geometry import Point
+#previously used, included for convenience
+from datetime import datetime
+#import mem_restrict  
+#vector math & comparisons
+import numpy as np
+
+
+
+class Main():
+    
+    def __init__(self):
+        #initializes with default values for the major parameters
+        #these can be changed here, or after creating an instance
+        
+        #radius commuters are willing to travel to charger (in miles)
+        self.r=1 
+        #aggregate to census tracts (vs. leave as blocks)
+        self.aggregate=True 
+        #number of stations allowed to open, if variable
+        self.B=50 
+        #add constraint for equity?
+        # 0: do not add
+        # 1: at least 40% of stations are placed in disadvantaged areas
+        # 2: at least 40% of commuters served are disadvantaged 
+        self.eqconstr=0 
+        #scale to only commuters who can't charge at home?
+        self.hc=True 
+
+        #input files:
+
+        #columns required:
+        # 'Customer ID' ids for viable commuter types (should be a valid key)
+        # 'Commuters' number of commuters estimated to exist of the commuter type
+        #  'Travel Distance' distance in miles the commuter type travels daily
+        # 'Commuters w/o Level 1' number of estimated commuters existing of the commuter type who do 
+        # not have access to home charging
+        # 'Home ID' id for home tract (fips id) of commuter type
+        #'Home ID' can be ommitted if not using equity constraints (self.eqconstr==0)
+        #my custfile also includes latitude/longitude and name data for both 'work' and 'home', which 
+        #may be useful for analysis of results
+        self.custfile='customers.csv'
+        #columns required: 'ID' --station ids for all viable station locations
+        #my statfile also includes latitude/longitude information, which may be useful for analysis
+        self.statfile='stations.csv'
+        #columns required: 'Customer ID','Station ID','Distance'
+        self.pairfile='pairs.csv'
+        #columns required: 
+        # '2020 ID' cencus fips id from 2020
+        # 'Disadvantaged' 0/1 whether community is disadvantaged
+        self.eqfile='communities.csv'
+
+        #default output files:
+        #station placements
+        self.zfile='AtlZ-2sd-model2.csv'
+        #commuter assignments
+        self.xfile='AtlX-2sd-model2.csv'
+        
+    
+    def ip(self, justModel=True, version=2):
+        #justModel: output created gurobipy model, do not run
+        #False: run model and save data to file instead
+
+        #version=model number to use
+        #1: serve as many customers as possible with limited stations
+        #2: serve all customers with as few stations as possible
+
+        #self.hc: if True, use only commuters who can't charge at home
+        #if False, serve all commuters, even home chargers
+
+        #self.eqconstr: #add constraint for equity?
+        # 0: do not add
+        # 1: at least 40% of stations are placed in disadvantaged areas
+        # 2: at least 40% of customers served are disadvantaged  
+        
+        
+        #read necessary data
+
+        #commuters w/o o-d numbers
+        if self.eqconstr>0: #home id for equity reference
+            cols=['Customer ID','Commuters', 'Travel Distance','Commuters w/o Level 1','Home ID']
+        else:
+            cols=['Customer ID','Commuters', 'Travel Distance','Commuters w/o Level 1']
+        custs= pd.read_csv(self.custfile, usecols=cols) 
+
+        #o-d numbers of commuters
+        cols=['Customer ID','Station ID','Distance']
+        pairs= pd.read_csv(self.pairfile,usecols=cols)   
+        pairs=pairs.loc[pairs['Distance']<=self.r]  
+        
+        m=Model()
+        
+        #list of indices for easier refernce
+        stations=pairs['Station ID'].unique()
+        stations=stations.tolist()
+        customers=custs['Customer ID'].unique()
+        customers=customers.tolist()
+
+
+        #xij: # customers of type i assigned to station j
+        x=dict()
+        for i in customers:
+            x[i]=dict()
+            S=pairs.loc[pairs['Customer ID']==i]
+            for index, row in S.iterrows():
+                x[i][row['Station ID']] =m.addVar(name='x_'+str(i)+str(row['Station ID']))        
+        
+        #zj: # chargers opened at location j
+        z = dict()
+        for j in stations:
+            z[j]=m.addVar(name='z_'+str(j),vtype=GRB.INTEGER)
+    
+        #each charger is assigned at most mm customers
+        mm=250*6*1 #num customer miles a single charger can serve:
+            #250 miles per charge
+            #1 hr to charge to full
+            #18 hours-ish each station runs
+            
+        #stations can charge at most mm * num chargers miles
+        for j in stations:
+            temp=pairs.loc[pairs['Station ID']==j]
+            sj=temp['Customer ID'].unique().tolist()
+            if len(sj)>0:
+                td=dict()
+                for i in sj:
+                    a=custs.loc[custs['Customer ID']==i]['Travel Distance']
+                    a=a.tolist()
+                    
+                    td[i]=a[0]
+                m.addConstr( (quicksum(td[i]*x[i][j] for i in sj)) <= z[j]*mm)
+                del td
+          
+        #at most B stations created
+        if version == 1:
+            m.addConstr( (quicksum(z[j] for j in stations)) <= self.B)
+        
+        #customer upper bounds
+        for i in customers:
+            if not self.hc:
+                upper=custs.loc[custs['Customer ID']==i]['Commuters']
+            else:
+                upper=custs.loc[custs['Customer ID']==i]['Commuters w/o Level 1']
+
+            upper=upper.tolist()
+            upper=upper[0]
+
+            upper=round(upper, 0)
+
+            temp=pairs.loc[pairs['Customer ID']==i]
+            si=temp['Station ID'].unique().tolist()
+            if len(si)>0:
+                if version ==2:
+                    #serve all customers
+                    m.addConstr( (quicksum(x[i][j] for j in si)) >= upper)
+                elif version ==1:
+                    #upper bound is # commuters that actually exist
+                    m.addConstr( (quicksum(x[i][j] for j in si)) <= upper)
+
+        #equity type of constraints
+        if self.eqconstr>0:
+            cols=['2020 ID','Disadvantaged']
+            df=pd.read_csv(self.eqfile,usecols=cols)
+
+            if self.eqconstr==1:
+                df.rename(columns={'2020 ID':'Station ID'}, inplace = True)
+                dStations=dict()
+                for j in stations:
+                    disad=df[df['Station ID']==j]['Disadvantaged'].iloc[0]
+                    if disad:
+                        dStations[j]=1
+                    else:
+                        dStations[j]=0
+                #at least 40% of stations must be in disadvantaged tracts
+                m.addConstr( (quicksum(z[j]*dStations[j] for j in stations)) >= 0.4*(quicksum(z[j] for j in stations)))
+            
+            elif self.eqconstr==2: #at least 40% of commuters served must live in disadvantaged tracts
+                df.rename(columns={'2020 ID':'Home ID'}, inplace = True)
+                dCust=dict()
+                df=custs.merge(df,on='Home ID',how='left')
+                for i in customers:
+                    disad=df[df['Customer ID']==i]['Disadvantaged'].iloc[0]
+                    if disad:
+                        dCust[i]=1
+                    else:
+                        dCust[i]=0
+                m.addConstr( (quicksum( (quicksum(x[i][j]*dCust[i] for j in x[i].keys())) for i in customers))>= 0.4*(quicksum( (quicksum(x[i][j] for j in x[i].keys())) for i in customers)))
+            del df
+        
+        #maximize customers served
+        if version ==1:
+            m.setObjective( (quicksum( (quicksum(x[i][j] for j in x[i].keys())) for i in customers)), GRB.MAXIMIZE)
+        
+        #minimize stations needed            
+        elif version ==2:
+            m.setObjective( (quicksum(z[j] for j in z.keys())), GRB.MINIMIZE)
+        
+        #solve to within 1% of optimality
+        m.setParam('MIPGap',0.01)
+        if justModel: 
+            #return the model object; do not solve it
+            return m
+        
+        del custs
+        del pairs
+
+        #actual run of model
+        m.optimize()
+        
+        #retrieve and save station placements
+        optzs=[['Name','i','z_i']]
+        for i in stations:
+            zstar=z[i].x
+            if zstar>0: #leave out unopened stations
+                temp=[z[i].VarName,i,zstar]
+                optzs.append(temp)
+        df=pd.DataFrame(optzs)
+        df.to_csv(self.zfile)
+        
+        #commuter assignments to stations
+        optxs=[['Name','i','j','x_ij']]
+        for i in customers:
+            for j in x[i].keys():
+                xstar=x[i][j].x
+                if not self.hc:
+                    xstar=math.floor(xstar)
+                if xstar>0: #leave out unassigned commuters
+                    optxs.append([x[i][j].VarName,i,j,xstar])
+        df=pd.DataFrame(optxs)
+        df.to_csv(self.xfile)
+
+        return m.ObjVal
+  
+
+
+
+'''
+#example
+main=Main()
+#here we can change parameters
+main.B=4115
+main.eqconstr=0
+#run model and output results into m.xfile,m.zfile
+m=main.ip(False, version = 2)
+#print optimal value
+print(m)'''
+
---
+++ preprocess.py
@@ -0,0 +1,429 @@
+# -*- coding: utf-8 -*-
+
+#distance calculations
+import math 
+#csv reading
+import pandas as pd 
+#not currently needed, here for convenience 
+#as I've used it before switching datasources
+import geopandas as gpd 
+#not currently needed, likewise here for convenience
+from shapely.geometry import Point 
+#not currently needed, here for convenience
+from datetime import datetime 
+#not currently needed, here for convenience
+import contextily as cx
+#distance calculations and df comparisons
+import numpy as np
+
+
+
+class Main():     
+
+    def __init__(self):
+
+        #main files:
+        #commuter info downloaded from LODES, unchanged. 
+        self.csvfile = "ga_od_main_JT00_2020.csv"
+        #customer info, with location data & some names for convenience
+        self.custfile='AtlCustomers.csv' #origin ID, dest ID, # commuters traveling between them, info on districts
+        #station info, made from the tract locations
+        self.statfile='AtlStations.csv' #station ID, location
+        #every valid pair of station & commuter; the function that creates this will 
+        #save it to this location; other functions will read this file
+        self.pairfile='AtlPairs.csv' #customer ID, station ID, distance
+
+        #parameters, set to recommended values:
+        #set this to True if working with smaller dataset as test set:
+        #only commuters who both live and work inside the perimeter are included
+        self.bitesize=False 
+        #if true, aggregate to tracts from blocks, and use tracks
+        #whenever relevant; otherwise solve in blocks
+        self.aggregate=True 
+
+        #additional inputs:
+        self.homefile='GAFPL2018.csv' #home statistics
+
+    def write_data(self):
+        #the main function you will want--
+        #filters LODES data into relevant commuter data, and, if self.aggregate,
+        #also aggregates these into census tracts from blocks
+        cust=self.format_cust()
+        cust.to_csv(self.custfile)
+        #uses self.custfile to create a list of potential station locations with IDs and 
+        #lat/long locations
+        stat=self.format_stat
+        stat.to_csv(self.statfile)
+        #there is one more file that the optimization will require, here referred to mostly as 
+        #pairfile, but it requires a fair bit of ram as written; 
+        #format_pair() documents the math performed
+    
+    def format_cust(self):
+        #take original LODES data at census block level from self.csvfile
+        #output origin info, destination info, and number of commuters traveling route
+
+        #if self.aggregate, output data will be aggregated into census tracts
+
+        #the location returned for the aggregated tracts is the linear average of the 
+        #latitude/longitudes of the centroids of the component blocks
+
+        #read & hold origin and destination info
+        origins=self.format_districts(orig=True)
+        dests=self.format_districts(orig=False)
+
+        #file with origin-destination commuter data
+        csvframe=pd.read_csv(self.csvfile)
+        
+        #origin, destination, and tot. num. commuters
+        csvframe=csvframe[['w_geocode','h_geocode','S000']]
+        
+        #rename columns for clarity
+        mapp=dict()
+        mapp['w_geocode']='Work ID'
+        mapp['h_geocode']='Home ID'
+        mapp['S000']='Commuters'
+        csvframe=csvframe.rename(columns=mapp)
+
+        if self.aggregate:
+
+            #change to tract id; set as string for concatenation
+            csvframe['Home ID'] = csvframe['Home ID'].astype(str)
+            csvframe['Home ID'] = csvframe['Home ID'].str[0:11] 
+            csvframe['Work ID'] = csvframe['Work ID'].astype(str)
+            csvframe['Work ID'] = csvframe['Work ID'].str[0:11]
+
+            #combined label for origin and destination as a string
+            csvframe['OrigDest'] = csvframe['Home ID']+csvframe['Work ID']
+            #put back the ids as integers
+            csvframe['Home ID'] = csvframe['Home ID'].astype('int64')
+            csvframe['Work ID'] = csvframe['Work ID'].astype('int64')
+
+            #sum over tract ids
+            commutes=csvframe[['OrigDest','Commuters']].groupby(['OrigDest']).sum()
+            #put new aggregated column in with IDs
+            csvframe=csvframe[['OrigDest','Home ID','Work ID']]
+            csvframe=csvframe.drop_duplicates()
+            csvframe=pd.DataFrame(csvframe)
+            csvframe=commutes.merge(csvframe,on='OrigDest',how='inner')
+            del commutes
+        
+        #add info on orig and destination tracts/blocks
+        odframe=origins.merge(csvframe,on='Home ID',how='inner')
+        del csvframe
+        odframe=dests.merge(odframe,on='Work ID',how='inner')
+        del origins
+        del dests
+        
+        odframe.index.name='Customer ID'
+
+        #distance between origin and destination
+        odframe['Travel Distance'] = odframe.apply(td,axis=1)
+        #is origin OR destination in ATL? (the set of data used in the paper)
+        odframe['Atlanta']=odframe.apply(bigAtl,axis=1)
+        #are origin AND destination in ATl? (a smaller set for testing)
+        odframe['Small Set']=odframe.apply(smallAtl,axis=1)
+        
+        if True: #output ONLY Atl (paper) data
+            odframe=odframe.loc[odframe['Atlanta']==1]
+        if self.bitesize: #output ONLY small testing data
+            odframe=odframe.loc[odframe['Small Set']==1]
+        
+        return odframe
+    
+    def format_stat(self):
+        #read in original LODEs data
+        #output list of potential stations and locations (one per tract/block)
+
+        #if self.aggregate, aggregate into tracts as with commuters
+
+        stations=self.format_districts(station=True)
+        stations.index.name='ID'
+
+        
+        
+        if True: #output only stations near Atl
+            stations['Atlanta']=stations.apply(nearAtl,axis=1)
+            stations=stations.loc[stations['Atlanta']==1]
+        else: #output all stations w/ additional binary column =1 if in Atl
+            stations['Atlanta']=stations.apply(statAtl,axis=1)
+        
+        return stations
+
+        
+    def format_districts(self,orig=True,station=False):
+        #pulls location and name data from crosswalk file
+        #outputs ID, tract/block info
+        
+        #orig: output w/ column labels for 'home'
+        #false: output w/ column labels for 'work'
+        #station: disregard 'origin'; output w/ column labels for 'station'
+        #a silly way to handle column names but if it works, don't break it ;)
+        #this isn't a function you're likely to call directly
+
+        #start: location for col labels
+        if orig:
+            start='Home'
+        else:
+            start='Work'  
+            
+        if station:
+            start='Station'
+        
+        #aggregate--use tract info; else use block info
+        if self.aggregate:
+            cols=['trct']
+        else:
+            cols=['tabblk2020']
+        #name for tract location, long and lat, name of county
+        cols=cols+['stwibname','blklondd','blklatdd','ctyname']
+        
+        #file with geodata for census tracts
+        xwalk = "ga_xwalk.csv"
+        districts = pd.read_csv(xwalk, usecols=cols)
+
+        #rename columns before merging for clarity
+        mapp=dict()
+        names=[start+' ID',start+' Name',start+' Longitude',start+' Latitude',start+' County']
+        ID,sd,x,y,cty=names
+        if self.aggregate:
+            mapp['trct']=ID
+        else:
+            mapp['tabblk2020']=ID
+        mapp['stwibname']=sd
+        mapp['blklondd']=x
+        mapp['blklatdd']=y
+        mapp['ctyname']=cty
+        districts=districts.rename(columns=mapp)
+        districts=districts[names]
+        districts[ID]=districts[ID].astype('int64')
+
+        #aggregation into tracts
+        if self.aggregate:
+            districts[ID] = districts[ID].astype(str)
+            districts[ID]=districts[ID].str[0:11]
+            districts[ID] = districts[ID].astype('int64')
+            districts=districts.groupby(ID).mean()
+            districts=pd.DataFrame(districts)
+            
+        return districts
+    
+    def format_pair(self):
+        #take customer info and station info from self.custfile and self.statfile
+        #output cust ID, station ID, and distance between every pair of cust and stat id in both files
+        #into self.pairfile
+        #(note that this does not filter, so do not expect it to)
+
+        #this is too large to run with the paper's dataset on my run-of-the-mill laptop;
+        #I performed the same math, but with multithreading, on a stronger computer
+
+        #this does, however, work for smaller datasets to generate a testing set
+        #and is included here both for that purpose and as documentation
+        
+        #read from (generated, not original) csv the lats & longs
+        cols=['Customer ID','Work Latitude','Work Longitude','Home Latitude','Home Longitude']
+        custs=pd.read_csv(self.custfile, usecols=cols)
+        #likewise for stations
+        cols=['ID','Station Latitude','Station Longitude']
+        stats=pd.read_csv(self.statfile,usecols=cols)
+        stats.rename(columns={'ID':'Station ID'}, inplace = True)
+        
+        #combine into commuters x stations
+        pairs=custs.merge(stats,how='cross')
+        
+        #distance calculation
+        pairs['Distance']=pairs.apply(pairDist,axis=1)
+        #removing excess columns (lat and long) to reduce final csv size
+        pairs=pairs[['Customer ID','Station ID','Distance']]
+
+        return pairs
+    
+
+
+    def home_charging(self):
+        #take premade customer file, add home charging percentages, output modified dataframe
+
+        #pull just the home info--dataframe with 
+        #tract id, type of building, # units
+        #left out some columns so this will have tracts split into several entries
+        cols=['FIP','BLD','UNITS']
+        df=pd.read_csv(self.homefile,usecols=cols)
+
+        #% of customers who do NOT have home charging access
+        apt=1-0.091
+        percents={
+            '1 UNIT DETACHED':1-0.783, 
+            '1 UNIT ATTACHED':1-0.076,
+            '2 UNIT': apt,
+            '3-4 UNIT':apt,
+            '5-9 UNIT': apt,
+            '10-19 UNIT': apt,
+            '20-49 UNIT': apt,
+            '50+ UNIT': apt,
+            'BOAT RV VAN':0,
+            'MOBILE TRAILER':0
+        }
+
+        #weighted = number of units without access to home charging
+        df['Weighted']=df['UNITS']
+        for (build,perc) in percents.items():
+            df.loc[df['BLD']==build,'Weighted']=df[df['BLD']==build]['UNITS']*perc
+
+        #leave out 'other' from the unit types
+        df.loc[~df['BLD'].isin(percents.keys()),'Weighted']=0
+
+        #combine entries for ea. tract
+        dfagg=df[['FIP','UNITS','Weighted']].groupby(['FIP']).sum().reset_index()
+
+        #divide to get percentage for output file
+        dfagg['Percent w/o Level 1']=dfagg['Weighted']/dfagg['UNITS']
+        print(dfagg['Percent w/o Level 1'].min(), dfagg['Percent w/o Level 1'].max())
+
+        #remove 'weighted' and 'units' as we will not need them
+        df=dfagg[['FIP','Percent w/o Level 1']]
+        df.rename(columns={'FIP':'Home ID'}, inplace = True)
+
+        #combine with original cust file
+        cdf=pd.read_csv(self.custin)
+        cdf=cdf.merge(df,on='Home ID',how='left')
+
+        #if no data, make sure it's nan (some are 0s)
+        cdf['Percent w/o Level 1'].replace(0,np.nan, inplace=True)
+        #calc avg percent without charging
+        tdf=cdf
+        tdf['Weighted']=tdf['Commuters']*tdf['Percent w/o Level 1']
+        totc=tdf.loc[~tdf['Percent w/o Level 1'].isna(),'Commuters'].sum()
+        avgpercent=tdf['Weighted'].sum()/totc
+        del tdf
+        #replace empty entries (should now all be nan) with avg percent
+        cdf['Percent w/o Level 1'].replace(np.nan,avgpercent, inplace=True)
+        cdf['Commuters w/o Level 1']=cdf['Commuters']*cdf['Percent w/o Level 1']
+
+        return cdf
+    
+def lldist(lat1, long1, lat2, long2):
+
+    # Convert latitude and longitude to
+    # spherical coordinates in radians.
+    degrees_to_radians = math.pi/180.0
+
+    # phi = 90 - latitude
+    phi1 = (90.0 - lat1)*degrees_to_radians
+    phi2 = (90.0 - lat2)*degrees_to_radians
+
+    # theta = longitude
+    theta1 = long1*degrees_to_radians
+    theta2 = long2*degrees_to_radians
+
+    # Compute spherical distance from spherical coordinates.
+    
+    # For two locations in spherical coordinates
+    # (1, theta, phi) and (1, theta', phi')
+    # cosine( arc length ) =
+    # sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+    # distance = rho * arc length
+    
+    cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) +
+    math.cos(phi1)*math.cos(phi2))
+    if cos>1:
+        cos=1
+    arc = math.acos( cos )
+    arc=arc*3958.8
+
+    return arc
+
+def statAtl( row ):
+    #simple function: is a given station (in a dataframe with correct col names) in Atl? 0/1
+    olon=row['Station Longitude']
+    olat=row['Station Latitude']
+  
+    return inAtl(olat,olon)
+    
+
+def inAtl(lat,lon):
+    #is a given latitude/longitude pair inside the I-285 perimeter rectangle? 0/1
+    lonmin=-84.505798
+    lonmax=-84.227909
+    latmax=33.923198
+    latmin=33.613440
+    '''
+    latmax=33.786307
+    lonmin=-84.411681
+    latmin=33.740897
+    lonmax=-84.379394'''
+    if lat>=latmin and lat <= latmax and lon >= lonmin and lon <= lonmax:
+        return 1
+    else:
+        return 0
+
+def td( row ):
+    #calculates the min distance between a station and a commuter's work and home locations 
+    #from a dataframe row with correct column names
+    ox=row['Home Longitude']
+    oy=row['Home Latitude']
+    dx=row['Work Longitude']
+    dy=row['Work Latitude']
+  
+    td=abs(lldist(oy,ox,dy,dx))
+    td=math.ceil(td)
+    td=2*td+5
+    return td
+
+def bigAtl( row ):
+    #is a commuter's home or work location in Atl? 0/1
+    #given the commuter's row in a dataframe
+    olon=row['Home Longitude']
+    olat=row['Home Latitude']
+    dlon=row['Work Longitude']
+    dlat=row['Work Latitude']
+  
+    if inAtl(olat,olon)==1 or inAtl(dlat,dlon)==1:
+        return 1
+    else:
+        return 0
+
+def smallAtl( row ):
+    #are a commuter's home and work locations both in Atl? 0/1
+    #given the associated row in a dataframe
+    olon=row['Home Longitude']
+    olat=row['Home Latitude']
+    dlon=row['Work Longitude']
+    dlat=row['Work Latitude']
+  
+    if inAtl(olat,olon)==1 and inAtl(dlat,dlon)==1:
+        return 1
+    else:
+        return 0
+    
+def pairDist(row):
+    #calculate the distnace between a commuter and a station, given the associated row in 
+    #a dataframe
+    olon=row['Home Longitude']
+    olat=row['Home Latitude']
+    dlon=row['Work Longitude']
+    dlat=row['Work Latitude']
+    slat=row['Station Latitude']
+    slon=row['Station Longitude']
+    
+    #distance to station for commuter is mininmum of distance to station from home and from work
+    d1=lldist(olat, olon, slat, slon)
+    d2=lldist(dlat,dlon,slat,slon)
+    d= min(d1,d2)
+    d=math.ceil(d)
+    return d
+
+def nearAtl(row):
+    #is a station near the centerpoint of the I-285 perimeter? 0/1
+    alat=33.769849341009376
+    alon=-84.3899233590007
+    slon=row['Station Longitude']
+    slat=row['Station Latitude']
+    d=lldist(alat, alon, slat, slon)
+    if d>50:
+        return 0
+    else:
+        return 1
+    
+    
+main=Main()
+hcdf=main.home_charging()
+hcdf.to_csv(main.custout)
